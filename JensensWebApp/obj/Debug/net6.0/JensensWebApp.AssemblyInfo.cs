0d871ade2db46cc032d54d8c50afd0afe7af7de1
lwcsharp <lwcsharp@gmail.com>
2024-06-05 23:11:34 +0200

    fixed warning CS8618:

--- JensensWebApp/obj/Debug/net6.0/JensensWebApp.AssemblyInfo.cs
+++ JensensWebApp/obj/Debug/net6.0/JensensWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JensensWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5620b79064cc2e9fd9012763038560fd67c1f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cada0a4d6d338a8ea463150d4d8231ee732da761")]
 [assembly: System.Reflection.AssemblyProductAttribute("JensensWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JensensWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
